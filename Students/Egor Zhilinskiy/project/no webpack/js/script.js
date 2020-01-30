7c436da2ce888274754471d836aeab6e5fd0e086
gek-finn <55052551+gek-finn@users.noreply.github.com>
2020-01-31 01:08:22 +0200

    Выполнены задания 1-3,  собран проект на webpack

--- Students/Egor Zhilinskiy/project/no webpack/js/script.js
+++ Students/Egor Zhilinskiy/project/no webpack/js/script.js
@@ -1,17 +1,65 @@
+
 const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
-];
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ]
 
+        class Products {
+            constructor (array) {
+                this.products = array
+                this.container = null
+                this._init ()
+            }
 
-const renderGoodsItem = (title, price) => {return `<div class ="goods-item"><img src ="img/placeholder.png" class = "goods-img"><h3>${title}</h3><p>${price}</p></div>`;
-};
+            _init () {
+                this.container = document.querySelector('.goods-list')
+                //render
+                this.render ()                
+            }
+            render () {
+                let str = ''
+                this.products.forEach(element => {
+                  str += this.createProduct (element)  
+                })
+                //str = '<div /><div /><div /><div /><div /><div />'
+                this.container.innerHTML = str //произойдет рендер
+            }
+            createProduct (prod) {
+                return `
+                <div class="goods-item">
+                <img src ="img/placeholder.png" class = "goods-img">
+                    <h3>${prod.title}</h3>
+                    <p>${prod.price}</p>
+                </div>
+                `
+            }
+            totalSum() {
+                let sum = this.products.reduce((accum,item) => {
+                    if (item.price) accum += item.price;
+                    return accum;    
+                    }, 0);
+                document.querySelector('.header-inner').innerHTML = `Сумма всех товаров:
+                 ${sum}`;
+            }
+        }
+        
+        let catalog = new Products (goods)
+        catalog.totalSum()
 
-const renderGoodsList = (list )=> {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join("");
-}
+        class Cart extends Products {
+            constructor(props) {
+                super(props);                
+            }
+            clean() {}
+            incGood() {}
+            decGood() {}
+        }
 
-renderGoodsList(goods);
+        class CartItem extends Products {
+            constructor(props) {
+                super(props);                
+            }
+            delete() {}
+        }
