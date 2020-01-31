b30bfacec0c765412e6ca1a89f4696ad89808a57
Elya <elfucio@gmail.com>
2020-02-01 00:29:30 +0300

    add burger project

---
+++ Students/Elya Iskhakova/project/webpack/webpack.config.js
@@ -0,0 +1,21 @@
+let HtmlPlugin = require ('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            
+            }
+        ]
+    },
+    plugins: [
+        new HtmlPlugin ({
+            template: './src/public/index.html'
+        })
+    ]
+};
