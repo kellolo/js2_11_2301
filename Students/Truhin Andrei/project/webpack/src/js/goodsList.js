a4bb1b6ec8a12854173119dd1a322abcba09045f
truhin-andrei <truhin-andrei@bk.ru>
2020-02-04 23:10:57 +0700

    feat: get goods list data

--- Students/Truhin Andrei/project/webpack/src/js/goodsList.js
+++ Students/Truhin Andrei/project/webpack/src/js/goodsList.js
@@ -1,64 +1,49 @@
 import GoodsItem from "./goodsItem";
 
-import makeGETRequest from './serverData';
+import makeGETRequest from "./serverData";
 
+export default class GoodsList {
+  constructor(image, prices, ids, items) {
+    this.image = image;
+  }
 
-export default class GoodsList{
-    constructor(image, prices, ids, items) {
-       
-        this.image = image;
-        this.prices = prices;
-        this.ids = ids;
-        this.items = items;
-        this.list = null;
-        
-    }
-   
-    fetchData () {
-        return new Promise((res, rej) => {
-            makeGETRequest ('catalogData.json')
-            .then (goods => {
-                const items = JSON.parse(goods);
-                console.log (goods)
-                console.log (JSON.parse (goods))
-                let arr = [];
+  fetchData() {
+    return new Promise((res, rej) => {
+      makeGETRequest("catalogData.json")
+        .then(goods => {
+          const items = JSON.parse(goods);
+          let arr = [];
 
-                items.forEach(good => {
-                    const newItem = new GoodsItem(this.image, good.price, good.id_product, good.product_name)
-                    arr.push(newItem.createProduct());
-                });
-                this.list = arr;
-                res(arr);
-                console.log (5,arr)
-            })
-            .catch (err => {
-                console.log (err)
-            })
+          items.forEach(good => {
+            const newItem = new GoodsItem(
+              this.image,
+              good.price,
+              good.id_product,
+              good.product_name
+            );
+            arr.push(newItem.createProduct());
+          });
+          res(arr);
         })
-    // let arr = [];
-    
-    // for (let i = 0; i < this.items.length; i++) {
-    //     const newItem = new GoodsItem(this.image, this.prices[i], this.ids[i], this.items[i])
-    //     arr.push(newItem.createProduct());
-    // }
-    // return arr;
-};
+        .catch(err => {
+          rej(err);
+          console.log(err);
+        });
+    });
+  }
 
-   
+  //рендер списка товаров (каталога)
+  renderProducts(arr) {
+    let str = "";
+    for (let item of arr) {
+      str += item.createTemplate();
+    }
+    document.querySelector(".products").innerHTML = str;
+  }
 
-//рендер списка товаров (каталога)
-renderProducts () {
-    let arr = this.list;
-    let str = '';
-    console.log (6,arr)
-    // for(let item of arr) {
-    //     str += item.createTemplate()
-    // }
-    document.querySelector('.products').innerHTML = str;
-};
-
-goodsListPrice(){
-   return this.prices.reduce(function (acc, price){return acc + price;
-}, 0);
-}
+  goodsListPrice() {
+    return this.prices.reduce(function(acc, price) {
+      return acc + price;
+    }, 0);
+  }
 }
