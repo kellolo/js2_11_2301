13e893a01a1324fcaee452fcdf145b0077ee8743
RomanOt <vatokato@gmail.com>
2020-02-10 00:37:53 +0500

    lesson 4 hw. Добавление удаление из корзины, подсчет итоговых сумм

---
+++ Students/Otstavnov Roman/project/src/js/settings.js
@@ -0,0 +1,7 @@
+const SETTINGS = Object.freeze({
+  image: 'https://placehold.it/200x150',
+  cartImage: 'https://placehold.it/100x80',
+  API: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
+});
+
+export default SETTINGS;
