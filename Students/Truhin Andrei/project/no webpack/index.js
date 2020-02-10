efe179660dfa979c5e29ef59ff3f340a0a3ca2a0
truhin-andrei <truhin-andrei@bk.ru>
2020-02-11 00:01:08 +0700

    project on vue

--- Students/Truhin Andrei/project/no webpack/index.js
+++ Students/Truhin Andrei/project/no webpack/index.js
@@ -7,23 +7,66 @@ let app = new Vue ({
         url_basket: '/getBasket.json',
         url_catalog: '/catalogData.json',
         items: [],
-        cart: []
+        cart: [],
+        isCartVisible: false,
+        totalSum: 0,
+        searchLine: '',
+        filteredItems: []
 
     },
 
     methods: {
         toggleCartVisibility(){
-            document.querySelector('.cart-block').classList.toggle('invisible');
+           //document.querySelector('.cart-block').classList.toggle('invisible');
+           this.isCartVisible = !this.isCartVisible;
         },
         getData (url) {
             return fetch(this.API+url)
                     .then(d => d.json())
-                    // .then(parsedData => { this.items = parsedData })
-                    // .finally(() => {
-                    //   //  console.log(document.querySelector('.products'))
-                    // })
         },
-        
+        addItem(item){
+            let serverResponse200
+            this.getData(this.API + '/addToBasket.json')
+                .then (response => { serverResponse200 = response })
+                .finally (() => {
+                    let find = this.cart.find(el => el.id_product === item.id_product);
+                    if (!find) {
+                        this.cart.push({
+                            id_product: item.id_product,
+                            product_name: item.product_name,
+                            price: +item.price,
+                            quantity: 1
+                        });
+                        this.totalSum += +item.price;
+                    } else {
+                        find.quantity++;
+                        this.totalSum += find.price;
+                    }                  
+                })
+        },
+
+        removeItem(item){
+            let serverResponse200
+            this.getData(this.API + '/deleteFromBasket.json')
+                .then (response => { serverResponse200 = response })
+                .finally (() => {
+                    if (serverResponse200) {
+                        let find = this.cart.find(element => element.id_product === item.id_product);
+                        if (find.quantity > 1) {                
+                            find.quantity--;
+                            this.totalSum -= find.price;
+                        } else {
+                            this.totalSum -= find.price;
+                            this.cart.splice(this.cart.indexOf(find), 1);
+                        }
+                        
+                    }
+                })
+        },
+        filterGoods(){
+            this.filteredItems = this.items.filter(item => item.product_name.toLowerCase().indexOf(this.searchLine.toLowerCase()) +1)
+            console.log(this.filteredItems)
+        },
     },
 
     computed: {
@@ -34,6 +77,7 @@ let app = new Vue ({
         this.items = await this.getData(this.API+this.url_catalog);
         dataCart = await this.getData(this.API+this.url_basket);
         this.cart = await dataCart.contents;
-        console.log( this.cart)
+        this.totalSum = await this.cart.reduce((acc,item) => acc+item.price* item.quantity,0)
+        this.filteredItems = await this.items
     }
 })
