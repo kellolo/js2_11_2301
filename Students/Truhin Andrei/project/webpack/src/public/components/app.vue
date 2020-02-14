2ba1c8ce70ee19b18fcc5345d9efc5439c49873e
truhin-andrei <truhin-andrei@bk.ru>
2020-02-14 22:11:35 +0700

    realization through vue components

--- Students/Truhin Andrei/project/webpack/src/public/components/app.vue
+++ Students/Truhin Andrei/project/webpack/src/public/components/app.vue
@@ -1,47 +1,40 @@
 <template>
-    <div>
-        <header>
-            <div class="logo">E-shop</div>
-            <div class="cart">
-                <form action="#" class="search-form">
-                    <input type="text" class="search-field">
-                    <button class="btn-search" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </form>
-                <button class="btn-cart" type="button" @click="isCartVisible = !isCartVisible">Корзина</button>   
-                <cart v-show="isCartVisible"/>
-                <!--Cart-->
-            </div>
-        </header>
-        <main>
-            <catalog />
-            <!--Catalog-->
-        </main>
-    </div>
+  <div>
+    <header>
+      <div class="logo">E-shop</div>
+      <div class="cart">
+        <searchForm />
+        <button class="btn-cart" type="button" @click="isCartVisible = !isCartVisible">Корзина</button>
+        <cart v-show="isCartVisible" />
+        <!--Cart-->
+      </div>
+    </header>
+    <main>
+      <catalog />
+      <!--Catalog-->
+    </main>
+  </div>
 </template>
 
 <script>
-
-import catalog from './Catalog.vue'
-import cart from './Cart.vue'
+import catalog from "./Catalog.vue";
+import cart from "./Cart.vue";
+import searchForm from "./SearchForm.vue";
 
 export default {
-    data() {
-        return {
-        isCartVisible: false,
-            }
-        },
-    methods: {
-        getData (url) {
-            return fetch(url)
-                    .then(d => d.json())
-        }
-    },
-    components: { catalog, cart }
-}
+  data() {
+    return {
+      isCartVisible: false
+    };
+  },
+  methods: {
+    getData(url) {
+      return fetch(url).then(d => d.json());
+    }
+  },
+  components: { catalog, cart, searchForm }
+};
 </script>
 
 <style>
-    
 </style>
