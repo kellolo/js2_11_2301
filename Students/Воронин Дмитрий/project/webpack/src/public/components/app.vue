38f3d644d3f0086f517b95001470fdb0d752abbe
jackson070896 <jackson070896@yandex.ru>
2020-02-22 17:28:16 +0700

    HW 22.02.2020

--- Students/Воронин Дмитрий/project/webpack/src/public/components/app.vue
+++ Students/Воронин Дмитрий/project/webpack/src/public/components/app.vue
@@ -30,7 +30,33 @@ export default {
     methods: {
         getData (url) {
             return fetch(url)
-                    .then(d => d.json())
+                .then(d => d.json())
+        },
+        postData (url, data) {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }).then(d => d.json())
+        },
+        putData (url, data) {
+            return fetch(url, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }).then(d => d.json())
+        },
+        deleteData (url) {
+            return fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(d => d.json())
         },
     },
     mounted() {
