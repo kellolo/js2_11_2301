b207e58b8d40187366c4a87b8bc63cfa14b6b044
midavp <midavp@list.ru>
2020-02-22 17:21:46 +0400

    8- DS_neDodelano chast 1

---
+++ Students/Pogosov Vadim/proect/vue 8/src/public/components/CartItem.vue
@@ -0,0 +1,42 @@
+<template>
+   
+        <!--<div v-for="el of cartitems">-->
+            <div class="cart-item">
+                <div class="product-bio">
+                    <img v-bind:src="cartimg" alt="Some image">
+                    <div class="product-desc">
+                        <p class="product-title">{{cartitem.product_name}}</p>
+                        <p class="product-quantity">Quantity: {{cartitem.quantity}}</p>
+                        <p class="product-single-price">${{cartitem.price}} each</p>
+                    </div>
+                </div>
+                <div class="right-block">
+                    <p class="product-price">${{cartitem.quantity*cartitem.price}}</p>
+                    <button class="del-btn" @click="del_btnClickitem(cartitem)">&times;</button>
+                </div>
+            </div>
+        
+        <!--<hr><p class="product-title">Сумма товаров: ${{cart_summa}}</p>-->
+   
+</template>
+
+<script>
+export default {
+    props: {
+        cartitem: {type: Object},
+    },
+    data() {
+        return {
+            cartimg: 'https://placehold.it/100x80',
+            urlGetData: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+        }
+    },
+    methods: {
+        del_btnClickitem(cartitem) {
+            console.log(this)
+            this.$parent.del_btnClick(cartitem)
+        }
+    }
+    
+}
+</script>
---
+++ Students/Pogosov Vadim/proect/vue 8/src/server/server.js
@@ -0,0 +1,75 @@
+
+let express = require('express')
+let fs = require ('fs')
+// let bodyParser = require('body-parser')
+let cart = require('./cart')
+let writer = require('./writer')
+
+let server = express ()
+server.use (express.json())
+
+server.use ('/', express.static('./src/static'))
+
+// server.use(bodyParser.json())
+
+server.get ('/catalog', (req, res) => {
+    fs.readFile ('./src/server/database/catalog.json', 'utf-8', (err, data) => {
+        if (!err) {
+            res.send (data)
+        } else {
+            res.sendStatus (404, JSON.stringify ({result: 0}))
+        }
+    })
+})
+
+server.get ('/cart', (req, res) => {
+    fs.readFile ('./src/server/database/cart.json', 'utf-8', (err, data) => {
+        if (!err) {
+            res.send (data)
+        } else {
+            res.sendStatus (404, JSON.stringify ({result: 0}))
+        }
+    })
+})
+
+server.post('/cart', (req, res) => {
+    let path =  './src/server/database/cart.json'
+      fs.readFile(path, 'utf8', (err, data) => {
+        if (!err) {
+            let {newCart, name} = cart.add(req, JSON.parse(data))
+            writer(path, JSON.stringify(newCart, null, ' '), res, {name:name, action:'add'})
+            
+
+        } else {
+            res.sendStatus (500, JSON.stringify ({result: 0}))
+        }
+        
+    })
+  })
+
+  server.put('/cart/:id', (req, res) => {
+    let path =  './src/server/database/cart.json'
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (!err) {
+          let {newCart, name} = cart.change(req, JSON.parse(data))
+          writer(path, JSON.stringify(newCart, null, ' '), res, {name:name, action:'change'})
+        } else {
+          res.sendStatus (500, JSON.stringify ({result: 0}))
+      }
+      })
+  })
+
+  server.delete('/cart/:id', (req, res) => {
+    let path =  './src/server/database/cart.json'
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (!err) {
+          let {newCart, name} = cart.delete(req, JSON.parse(data))
+          writer(path, JSON.stringify(newCart, null, ' '), res, {name:name, action:'del'})
+        } else {
+          res.sendStatus (500, JSON.stringify ({result: 0}))
+      }
+      })
+  })
+
+server.listen (8080, () => {console.log ('listening at port 8080')})
+
