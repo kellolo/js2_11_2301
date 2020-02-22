f13ef4f70e538d93042702b4c2f393ad0fb215b1
AndreySalnikov99 <andrew_salnikov@mail.ru>
2020-02-22 19:20:05 +0300

    new8

---
+++ Students/Andrey Salnikov/project/8/shop-cli/src/components/CatalogItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="product-item">
+        <img :src="img" alt="Some img">
+        <div class="desc">
+            <h3>{{ item.product_name }}</h3>
+            <p>{{ item.price }} $</p>
+            <button class="buy-btn" @click="$root.$children[0].$children[2].$refs.cart.addToCart (item)">Купить</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {type: Object},
+    },
+    data() {
+        return {
+            img: 'https://placehold.it/200x150'
+        }
+    },
+}
+</script>
