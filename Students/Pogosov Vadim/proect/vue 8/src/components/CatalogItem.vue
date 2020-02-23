14b6a69b4a88f51775b228d2dc035879b707c761
midavp <midavp@list.ru>
2020-02-23 20:24:28 +0400

    8- DS_Sdelanono

---
+++ Students/Pogosov Vadim/proect/vue 8/src/components/CatalogItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="product-item">
+        <img v-bind:src="img" alt="Some img">
+        <div class="desc">
+            <h3>{{item.product_name}}</h3>
+            <p>{{item.price}} $</p>
+            <button class="buy-btn" @click="add_btnClickitem (item)">Купить</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {type: Object},
+    },
+    data() {
+        return {
+            img: 'https://placehold.it/200x150'
+        }
+    },
+    methods: {
+        add_btnClickitem(em) {
+            console.log(this.$root.$children[0])
+            
+            this.$root.$children[0].$children[2].$refs.cart.add_btnClick(em)
+
+        }
+    }
+    
+}
+</script>
