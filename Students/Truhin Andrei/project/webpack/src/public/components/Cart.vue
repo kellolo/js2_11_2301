dfafcc8af62be37905bb20ae6204f491e972db9b
truhin-andrei <truhin-andrei@bk.ru>
2020-03-01 19:32:28 +0700

    lesson 8

--- Students/Truhin Andrei/project/webpack/src/public/components/Cart.vue
+++ Students/Truhin Andrei/project/webpack/src/public/components/Cart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cart-block invisibleq">
     <cartItem v-for="item of cart" :key="item.id_product" :cartItem="item" />
-    <span>Total: {{this.getTotalSum()}}</span>
+    <span>Total: {{ this.getTotalSum() }}</span>
   </div>
 </template>
 
@@ -13,9 +13,7 @@ export default {
   data() {
     return {
       cart: [],
-       urlCart: '/api/cart',
-      // urlCart:
-      //   "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json"
+      urlCart: "/api/cart"
     };
   },
   mounted() {
@@ -25,51 +23,34 @@ export default {
   },
   methods: {
     removeItem(item) {
-      let serverResponse200;
-      this.$parent
-        .getData(
-          "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/deleteFromBasket.json"
-        )
-        .then(response => {
-          serverResponse200 = response;
-        })
-        .finally(() => {
-          if (serverResponse200) {
-            let find = this.cart.find(
-              element => element.id_product === item.id_product
-            );
-            if (find.quantity > 1) {
-              find.quantity--;
-            } else {
-              this.cart.splice(this.cart.indexOf(find), 1);
-            }
-          }
+      let find = this.cart.find(
+        element => element.id_product === item.id_product
+      );
+      if (find.quantity > 1) {
+        this.$parent.putData(`/api/cart/${item.id_product}`, { delta: -1 }).then(d => {
+          d.result ? find.quantity-- : console.log("error");
         });
+      } else {
+        this.$parent.deleteData(`/api/cart/${item.id_product}`).then(d => {
+          d.result
+            ? this.cart.splice(this.cart.indexOf(find), 1)
+            : console.log("error");
+        });
+      }
     },
     addItem(item) {
-      let serverResponse200;
-      this.$parent
-        .getData(
-          //'/api/addtocart',
-          "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/addToBasket.json",
-        )
-        .then(response => {
-          serverResponse200 = response;
-console.log(JSON.stringify(item))
-        })
-        .finally(() => {
-          let find = this.cart.find(el => el.id_product === item.id_product);
-          if (!find) {
-            this.cart.push({
-              id_product: item.id_product,
-              product_name: item.product_name,
-              price: +item.price,
-              quantity: 1
-            });
-          } else {
-            find.quantity++;
-          }
+      let find = this.cart.find(el => el.id_product === item.id_product);
+      if (!find) {
+        const ob = Object.assign({}, item, { quantity: 1 });
+
+        this.$parent.postData(`/api/cart`, ob).then(d => {
+          d.result ? this.cart.push(ob) : console.log("error");
         });
+      } else {
+        this.$parent.putData(`/api/cart/${item.id_product}`, { delta: 1 }).then(d => {
+          d.result ? find.quantity++ : console.log("error");
+        });
+      }
     },
     getTotalSum() {
       return this.cart.reduce(
